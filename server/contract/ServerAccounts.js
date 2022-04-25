049f82b21e25bc3af3c269c0130b8275df0de4aa
DonggyuLim <88867825+DonggyuLim@users.noreply.github.com>
2022-04-26 01:41:45 +0900

    CA .ENV 에 추가해야함.

--- server/contract/ServerAccounts.js
+++ server/contract/ServerAccounts.js
@@ -1,10 +1,11 @@
 import { ethers } from "ethers";
 import provider from "./provider.js";
 import abi from "./abi.js";
+import { config } from "../config.js";
 const privateKey =
   "3c9182c9c893b7320c8d65464e5693c261922d36ab1461c4c562b630b54592cc";
 
-const CA = "0x25326D3fb0a45469ecaBF9B7F2CEe9C3B49681f3";
+const CA = config.CA;
 const wallet = new ethers.Wallet(privateKey, provider);
 const erc20 = new ethers.Contract(CA, abi, wallet);
 
